b0568d11c8bc08690b7fe321f117eee84afc3491
Ondřej Habina <ondrej.habina@gmail.com>
2023-01-11 23:27:02 +0100

    preparing first components

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,13 +10,13 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
 
+  //Input handling
   const [input, setInput] = React.useState('')
 
   function getCurrentInput(value) {
     const inputValue = value.target.value;
     setInput(inputValue)
   }
-
   
   function handleSubmit() {
     let submittedCity = input
@@ -28,11 +28,43 @@ export default function Home() {
       handleSubmit()
     }
   }
+  /////
+
+
+  function TemporalData(timeData) {
+    function getCurrentTime() {
+      const today = new Date()
+      const date = ''
+      const hours = today.getHours()
+      const minutes = today.getMinutes()
+    }
+    const time: string = getCurrentTime() //To be created
+    const date: string = '';
+    //need more time to come up with a solution for time that includes time being displayed immediately but also reusability of TemporalData component for the following days
+
+    return(
+      <></>
+    )
+  }
+
+  function PrimaryDataDisplay(primaryData) {
+
+    const temperature : string = primaryData.temp
+    const weatherCategory : string = primaryData.weatherCategory
+    const weatherDescription: string = primaryData.weatherDescription
+    
+
+    return (
+      <section>
+        
+      </section>
+    )
+  }
 
   return (
     <main>
       <input type={'text'} onChange={getCurrentInput} onKeyDown={(e) => handleEnterSubmit(e)}></input>
-      <button onClick={handleSubmit}></button>
+      <button onClick={handleSubmit}>Search</button>
       <div>Hello</div>
     </main>
   )
